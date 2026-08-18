b395f50e6c2367d588f9bbf770f9b97595e6d6ec
Alison Gray <argray@uw.edu>
2026-08-18 22:06:41 +0000

    Add line to return optimized parameters

--- scripts/fit_NPQ.py
+++ scripts/fit_NPQ.py
@@ -7,8 +7,10 @@ def fit_alpha_NPQ(prof_data):
 
     # NPQ = NPQ_max(1 - exp(-(PAR * alpha_NPQ) / NPQ_max))
     # fit NPQ_max and alpha_NPQ from PAR and NPQ data
-    popt, pcov = curve_fit(NPQ_func, prof_data.PAR, prof_data.NPQ)
-
+    param_opt, pcov = curve_fit(NPQ_func, prof_data.PAR, prof_data.NPQ)
+    # NPQ_max = param_opt[0]
+    # alpha_NPQ = param_opt[1]
+    return param_opt
 
 def NPQ_func(PAR, NPQ_max, alpha_NPQ):
     return NPQ_max * (1 - np.exp(-(PAR * alpha_NPQ)/NPQ_max)
