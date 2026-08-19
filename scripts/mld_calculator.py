3bec3f29062d0a7d2fd392059bb52ca030633b70
haydenleo <159191861+haydenleo@users.noreply.github.com>
2026-08-19 16:42:48 -0700

    Add files via upload

---
+++ scripts/mld_calculator.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+#calculate MLD for profiles
+def mld_calc(floatdata):
+    MLD=np.zeros(len(floatdata.N_PROF))
+    MLD[:]=np.nan
+    for i in floatdata.N_PROF:
+        prof=floatdata.isel(N_PROF=i)
+        densprof=gsw.density.sigma0(prof.PSAL,prof.TEMP).values
+        # refdens=densprof[np.abs(prof.PRES - 15).argmin()]
+        refdep=np.abs(prof.PRES - 10).argmin()
+        refdens=np.mean(densprof[0:refdep.values])
+        for g in range(refdep.values,len(densprof)):
+            dens=densprof[g]
+            densdiff=np.abs(refdens-dens)
+            if densdiff>0.03:
+                MLD[i]=prof.isel(N_LEVELS=g).PRES
+                break
+    return(MLD)
