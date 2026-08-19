999023a7e3c298bcaeffb660cafff1a58bcde2d8
Alison R. Gray <alisonrgray@users.noreply.github.com>
2026-08-19 16:31:05 -0700

    Update fit_npq.py

--- scripts/fit_npq.py
+++ scripts/fit_npq.py
@@ -3,7 +3,8 @@ import xarray as xr
 import numpy as np
 from scipy.optimize import curve_fit
 
-def fit_alpha_npq(ds):
+def fit_alpha_npq(ds, npq_tshld=0.1):
+
 
     dsn = ds.copy()
 
@@ -13,12 +14,18 @@ def fit_alpha_npq(ds):
     # extract par data
     par = dsn['DOWNWELLING_PAR']
 
-    # select only data where par > 0
-    npq = npq.where(par > 0)
+    # select only data where above npq depth
+    npq = npq()
+    par = par()
 
-    # Further constraints on the fits 
+# Further constraints on the fits 
     # 1. prescribing reasonable maximum bounds for aNPQ and NPQmax (0.1 and 20, respectively)
     # 2. manipulating PAR to yield a zero intercept between NPQ and PAR. This was achieved by subtracting the PAR value where NPQ first reached a low threshold, in this case 0.1.
+
+    # find par value where npq is less than npq threshold
+    par_idx = par.where(npq <= npq_tshld).argmin()
+
+    par = par - par(par_idx)
     
     # fit NPQ_max and alpha_NPQ from PAR and NPQ data
     param_opt, pcov = curve_fit(NPQ_func, par, npq)
