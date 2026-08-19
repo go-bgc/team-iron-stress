97b4919a833eaaec7694d1ae8815d153fe692a27
Alison Gray <argray@uw.edu>
2026-08-19 22:23:27 +0000

    Update script to use input data array

--- scripts/fit_npq.py
+++ scripts/fit_npq.py
@@ -3,17 +3,31 @@ import xarray as xr
 import numpy as np
 from scipy.optimize import curve_fit
 
-def fit_alpha_npq(prof_data):
+def fit_alpha_npq(ds):
 
-    # NPQ = NPQ_max(1 - exp(-(PAR * alpha_NPQ) / NPQ_max))
+    dsn = ds.copy()
+
+    # calculate npq as difference between fluo_npq and fluo_smooth
+    npq = dsn['fluo_npqc']-dsn['fluo_smooth']
+
+    # extract par data
+    par = dsn['DOWNWELLING_PAR']
+
+    # select only data where par > 0
+    npq = npq.where(par > 0)
+
     # fit NPQ_max and alpha_NPQ from PAR and NPQ data
-    param_opt, pcov = curve_fit(NPQ_func, prof_data.PAR, prof_data.NPQ)
+    param_opt, pcov = curve_fit(NPQ_func, par, npq)
     # NPQ_max = param_opt[0]
     # alpha_NPQ = param_opt[1]
-    return param_opt
+
+    dsn = dsn.assign({'npq_max': param_opt[0], 'alpha_npq': param_opt[1]})
+    
+    return dsn
 
 def npq_func(PAR, NPQ_max, alpha_NPQ):
     return NPQ_max * (1 - np.exp(-(PAR * alpha_NPQ)/NPQ_max))
 
+    
                       
     
