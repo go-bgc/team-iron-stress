75c5052fcd04461dd98a72af75fc296a7ab20437
Alison Gray <argray@uw.edu>
2026-08-20 17:41:30 +0000

    Fix bugs in fitting code, update notebook

--- scripts/fit_npq.py
+++ scripts/fit_npq.py
@@ -1,31 +1,38 @@
 import pandas as pd
 import xarray as xr
 import numpy as np
-from scipy.optimize import curve_fit
 
-def fit_alpha_npq(ds0, npq_tshld=0.1):
+def fit_alpha_npq(ds0, npq_thr=0.1):
 
     ds = ds0.copy()
 
     # calculate npq as difference between fluo_npq and fluo_smooth
-    npq = ds['fluo_npqc']-ds['fluo_smooth']
+    npq = (ds['fluo_npqc']-ds['fluo_smooth'])/ds['fluo_smooth']
+    ds['npq'] = npq
 
+    #max_cloud_elev = cloud.c.where(cloud > 0.6).max(dim="c")
+    
     # select only data where above npq depth
-    npq = npq.where(ds['PRES_ADJUSTED'] < ds['NPQ_depth'],drop=True)
-    par = ds['DOWNWELLING_PAR'].where(ds['PRES_ADJUSTED'] < ds['NPQ_depth'],drop=True)
+    #npq = npq.where(ds['PRES_ADJUSTED'] < ds['NPQ_depth'],drop=True)
+    #par = ds['DOWNWELLING_PAR'].where(ds['PRES_ADJUSTED'] < ds['NPQ_depth'],drop=True)
     
     # Constraint 2: subtract the PAR value where NPQ first reached a low threshold, in this case 0.1.
-    par_idx = par.where(npq < npq_tshld).argmin()
-    par = par - par(par_idx)
-    
+    #par_idx = par.where(npq < npq_tshld).argmin()
+    #par = par - par(par_idx)
+    par_thr = ds['DOWNWELLING_PAR'].where(npq < npq_thr).min(dim="N_LEVELS")
+    ds['par'] = ds['DOWNWELLING_PAR']-par_thr
+
     # fit NPQ_max and alpha_NPQ from PAR and NPQ data
     # Constraint 1: prescribe reasonable maximum bounds for aNPQ and NPQmax (0.1 and 20, respectively)
+    ds_param = ds['npq'].curvefit(
+        coords = ds.par,
+        func = npq_func,
+        reduce_dims="N_LEVELS",
+        bounds={"NPQ_max": (0, 20), "alpha_NPQ": (0, 0.1)},
+    )
 
-    param_opt, pcov = curve_fit(NPQ_func, par, npq, bounds=(0, [20., 0.1]))
-    # NPQ_max = param_opt[0]
-    # alpha_NPQ = param_opt[1]
-
-    ds = ds.assign({'NPQ_MAX': param_opt[0], 'ALPHA_NPQ': param_opt[1]})
+    ds['NPQ_max'] = ds_param.sel(param='NPQ_max').curvefit_coefficients
+    ds['alpha_NPQ'] = ds_param.sel(param='alpha_NPQ').curvefit_coefficients
     
     return ds
 
